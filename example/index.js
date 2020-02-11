63bf8e420acd1046fcd7b176ab8ec937410058c8
Mathieu ROBERT <mathieu.robert@yocto.re>
2020-02-11 10:33:05 +0400

    [fix](gitCard): update call to ingenico

--- example/index.js
+++ example/index.js
@@ -1,13 +1,20 @@
 'use strict';
 
 var ingenico  = require('../src/')();
+var _ = require('lodash');
 
-var config    = { uri : 'https://3p.ims-loyalty.de/opallight/vindemia' };
+var config    = {
+  staging : { uri : 'https://3pqat.ims-loyalty.de/prepaidplus/vindemia' },
+  production : { uri : 'https://3p.ims-loyalty.de/prepaidplus/vindemia' }
+};
+
+config = _.first(_.values(_.pick(config, _.toLower(process.env.NODE_ENV) || 'staging')));
+console.log('testing in with following env values : ', config);
 
 if (ingenico.prepare(config)) {
-  ingenico.giftCard().getBalance('CARD_NUMBER', 'PIN_CODE').then(function (response) {
-    console.log(response);
+  ingenico.giftCard().getBalance('6391999501000285673', '1404').then(function (response) {
+    console.log('response', response);
   }).catch(function (error) {
-    console.log(error);
+    console.log('error', error);
   });
 }
